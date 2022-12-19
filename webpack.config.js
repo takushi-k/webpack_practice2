8dc992b2c2d3c0ec9aa3206848c60126a168fd86
takushi <50552608+takushi-k@users.noreply.github.com>
2022-12-19 17:56:44 +0900

    install css-loader

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+console.log(path.resolve(__dirname, './dist'))
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  }
+
+}
